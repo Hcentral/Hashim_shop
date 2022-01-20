5764079cc370cc0adc14be81350ab74cf829eab4
Hashim Aliyu Hashim <97265295+Hashimscrew@users.noreply.github.com>
2022-01-20 11:09:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
-var notes = [];
-
-// Registering all the event handlers when the page loads
-document.addEventListener("DOMContentLoaded", event => {
-    if (localStorage.getItem("notes")) {
-        notes = JSON.parse(localStorage.getItem("notes"));
-    }
-    renderNotes();
- 
-    document.querySelector("form").addEventListener("submit", event => {
-        event.preventDefault();
-        const note = document.querySelector("textarea").value;
-        if (note.length==0) {
-            alert("You didn't input any content");
-        } else {
-            notes.push(note);
-            renderNotes();
-            save();
-            document.querySelector("textarea").value = "";
-        }
-    });
-
-
-    document.querySelector("#btnLearn").addEventListener("click", event => {
+document.querySelector("#btnLearn").addEventListener("click", event => {
         location.href = "https://frontendmasters.com";
     })
 
@@ -42,41 +19,9 @@ document.addEventListener("DOMContentLoaded", event => {
     })
 
     document.querySelector("#btnShare").addEventListener("click", event => {
-        let notesString = "";
-        for (let note of notes) {
-            notesString += note + " | "
-        }
         navigator.share({
             title: "Codepad",
-            text: notesString
+            text: "I visit this awesome app they have Nice products for sale"
         })
     })
 })
-
-// Render the notes on the DOM
-function renderNotes() {
-    const ul = document.querySelector("#notes");
-    ul.innerHTML = "";
-    notes.forEach( (note, index) => {
-        // Create the note LI
-        const li = document.createElement("li");
-        li.innerHTML = note;
-        // Delete element for each note
-        const deleteButton = document.createElement("a");
-        deleteButton.innerHTML = '<span class="icon">delete</span>';
-        deleteButton.addEventListener("click", event => {
-            if (confirm("Do you want to delete this note?")) {
-                notes.splice(index, 1);
-                renderNotes();
-                save();
-            }
-        });
-        li.appendChild(deleteButton);
-        ul.appendChild(li);
-    })
-}
-
-
-function save() {
-    localStorage.setItem("notes", JSON.stringify(notes));
-}
